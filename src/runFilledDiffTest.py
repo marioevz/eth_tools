18d025acf6b19b112551cdb3fe5f74202471be2d
marioevz <marioevz@gmail.com>
2021-11-02 23:16:16 +0000

    Fixed runFilledDiffTest

--- src/runFilledDiffTest.py
+++ src/runFilledDiffTest.py
@@ -52,170 +52,171 @@ err = False
 
 for test_name in filled_test.keys():
 
-    for tc in filled_test[test_name]:
+    for fork in filled_test[test_name]:
         
-        if tc == '_info':
+        if fork == '_info':
             continue
-        test_count += 1
-        tc = filled_test[test_name][tc]
-        block = tc["currentBlockNumber"]
-        tc_difficulty = tc["currentDifficulty"]
-        timestamp = tc["currentTimestamp"]
-        fork = tc["network"]
-        if not fork in fork_pushback_block_count:
-            raise Exception('Incorrect fork')
-        parent_diff = tc["parentDifficulty"]
-        parent_timestamp = tc["parentTimestamp"]
-        parent_uncle_count = tc["parentUncles"]
-
-        if parent_diff.startswith('0x'):
-            parent_diff = int(parent_diff, 16)
-        else:
-            parent_diff = int(parent_diff)
-
-        if parent_uncle_count.startswith('0x'):
-            parent_uncle_count = int(parent_uncle_count, 16)
-        else:
-            parent_uncle_count = int(parent_uncle_count)
-
-        if parent_timestamp.startswith('0x'):
-            parent_timestamp = int(parent_timestamp, 16)
-        else:
-            parent_timestamp = int(parent_timestamp)
-
-        if timestamp.startswith('0x'):
-            timestamp = int(timestamp, 16)
-        else:
-            timestamp = int(timestamp)
-
-        if block.startswith('0x'):
-            block = int(block, 16)
-        else:
-            block = int(block)
         
-        if tc_difficulty.startswith('0x'):
-            tc_difficulty = int(tc_difficulty, 16)
-        else:
-            tc_difficulty = int(tc_difficulty)
-        
-        emptyOmmersHash =    "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347"
-        nonEmptyOmmersHash = "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49348"
+        for tc in filled_test[test_name][fork]:
+            test_count += 1
+            tc = filled_test[test_name][fork][tc]
+            block = tc["currentBlockNumber"]
+            tc_difficulty = tc["currentDifficulty"]
+            timestamp = tc["currentTimestamp"]
+            if not fork in fork_pushback_block_count:
+                raise Exception('Incorrect fork')
+            parent_diff = tc["parentDifficulty"]
+            parent_timestamp = tc["parentTimestamp"]
+            parent_uncle_count = tc["parentUncles"]
 
-        # parent_uncle_count = hex(parent_uncle_hash).lower() != emptyOmmersHash.lower()
+            if parent_diff.startswith('0x'):
+                parent_diff = int(parent_diff, 16)
+            else:
+                parent_diff = int(parent_diff)
 
-        parent_uncle_hash = nonEmptyOmmersHash if parent_uncle_count else emptyOmmersHash
+            if parent_uncle_count.startswith('0x'):
+                parent_uncle_count = int(parent_uncle_count, 16)
+            else:
+                parent_uncle_count = int(parent_uncle_count)
 
-        adj_factor = max((2 if parent_uncle_count else 1) - ((timestamp - parent_timestamp) // 9), -99)
+            if parent_timestamp.startswith('0x'):
+                parent_timestamp = int(parent_timestamp, 16)
+            else:
+                parent_timestamp = int(parent_timestamp)
 
-        diff_minus_bomb = (parent_diff + (parent_diff // BLOCK_DIFF_FACTOR) * adj_factor)
+            if timestamp.startswith('0x'):
+                timestamp = int(timestamp, 16)
+            else:
+                timestamp = int(timestamp)
 
-        minimum_difficulty = 131072
+            if block.startswith('0x'):
+                block = int(block, 16)
+            else:
+                block = int(block)
+            
+            if tc_difficulty.startswith('0x'):
+                tc_difficulty = int(tc_difficulty, 16)
+            else:
+                tc_difficulty = int(tc_difficulty)
+            
+            emptyOmmersHash =    "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347"
+            nonEmptyOmmersHash = "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49348"
 
-        diff_minus_bomb = minimum_difficulty if diff_minus_bomb < minimum_difficulty else diff_minus_bomb
+            # parent_uncle_count = hex(parent_uncle_hash).lower() != emptyOmmersHash.lower()
 
-        periodCount = (block - fork_pushback_block_count[fork]) // 100000
+            parent_uncle_hash = nonEmptyOmmersHash if parent_uncle_count else emptyOmmersHash
 
-        if periodCount > 0:
-            bomb = int(2 ** (periodCount - 2))
-        else:
-            bomb = 0
+            adj_factor = max((2 if parent_uncle_count else 1) - ((timestamp - parent_timestamp) // 9), -99)
 
-        expected_diff = diff_minus_bomb + bomb
+            diff_minus_bomb = (parent_diff + (parent_diff // BLOCK_DIFF_FACTOR) * adj_factor)
 
-        if expected_diff != tc_difficulty:
-            failing_count += 1
-            expected_diff_count += 1
-            print("TC {} Fail: Expected difficulty is different from actual difficulty in test case (expected={} / test case={})".format(tc, hex(expected_diff), hex(tc_difficulty)))
-            pprint.pprint(tc)
-            pprint.pprint({
-                "currentNumber": str(block),
-                "currentTimestamp": str(timestamp),
-                "currentDifficulty": hex(tc_difficulty),
-                "parentTimestamp" : str(parent_timestamp),
-                "parentDifficulty" : hex(parent_diff),
-                "parentUncleHash": parent_uncle_hash,
-                "adjustmentFactor": adj_factor,
-                "difficultyMinusBomb": hex(diff_minus_bomb),
-                "periodCount": periodCount,
-                "bomb": bomb,
-            })
-            err = True
+            minimum_difficulty = 131072
 
-        alloc = {
-        "a94f5374fce5edbc8e2a8697c15331677e6ebf0b": {
-            "balance": "0x5ffd4878be161d74",
-            "code": "0x",
-            "nonce": "0xac",
-            "storage": {}
-        },
-        "0x8a8eafb1cf62bfbeb1741769dae1a9dd47996192":{
-            "balance": "0xfeedbead",
-            "nonce" : "0x00"
-        }
-        }
+            diff_minus_bomb = minimum_difficulty if diff_minus_bomb < minimum_difficulty else diff_minus_bomb
 
-        txs = []
+            periodCount = (block - fork_pushback_block_count[fork]) // 100000
 
-        env = {
-        "currentCoinbase": "0xc94f5374fce5edbc8e2a8697c15331677e6ebf0b",
-        "currentGasLimit": "0x750a163df65e8a",
-        "currentBaseFee": "0x500",
-        "currentNumber": str(block),
-        "currentTimestamp": str(timestamp),
-        "parentTimestamp" : str(parent_timestamp),
-        "parentDifficulty" : hex(parent_diff),
-        "parentUncleHash": parent_uncle_hash
-        }
+            if periodCount > 0:
+                bomb = int(2 ** (periodCount - 2))
+            else:
+                bomb = 0
 
-        env_out_path = '/dev/shm/env.json'
-        alloc_out_path = '/dev/shm/alloc.json'
-        txs_out_path = '/dev/shm/txs.json'
-        output_path = '/dev/shm/out.json'
+            expected_diff = diff_minus_bomb + bomb
 
-        with open(env_out_path, 'w') as f:
-            json.dump(env, f)
-        with open(alloc_out_path, 'w') as f:
-            json.dump(alloc, f)
-        with open(txs_out_path, 'w') as f:
-            json.dump(txs, f)
+            if expected_diff != tc_difficulty:
+                failing_count += 1
+                expected_diff_count += 1
+                print("TC {} Fail: Expected difficulty is different from actual difficulty in test case (expected={} / test case={})".format(tc, hex(expected_diff), hex(tc_difficulty)))
+                pprint.pprint(tc)
+                pprint.pprint({
+                    "currentNumber": str(block),
+                    "currentTimestamp": str(timestamp),
+                    "currentDifficulty": hex(tc_difficulty),
+                    "parentTimestamp" : str(parent_timestamp),
+                    "parentDifficulty" : hex(parent_diff),
+                    "parentUncleHash": parent_uncle_hash,
+                    "adjustmentFactor": adj_factor,
+                    "difficultyMinusBomb": hex(diff_minus_bomb),
+                    "periodCount": periodCount,
+                    "bomb": bomb,
+                })
+                err = True
 
-        if os.path.exists(output_path):
-            os.remove(output_path)
+            alloc = {
+            "a94f5374fce5edbc8e2a8697c15331677e6ebf0b": {
+                "balance": "0x5ffd4878be161d74",
+                "code": "0x",
+                "nonce": "0xac",
+                "storage": {}
+            },
+            "0x8a8eafb1cf62bfbeb1741769dae1a9dd47996192":{
+                "balance": "0xfeedbead",
+                "nonce" : "0x00"
+            }
+            }
 
-        proc_output = subprocess.DEVNULL
-        proc_err = subprocess.DEVNULL
-        if err:
-            proc_output = None
-            proc_err = None
+            txs = []
 
-        # Run EVM
-        command = ['evm', 't8n', 
-                    '--input.alloc='+alloc_out_path,
-                    '--input.txs='+txs_out_path,
-                    '--input.env='+env_out_path,
-                    '--output.result='+output_path,
-                    '--state.fork='+fork
-                ]
-        if err:
-            print(command)
-        subprocess.call(command, stdout=proc_output, stderr=proc_err)
+            env = {
+            "currentCoinbase": "0xc94f5374fce5edbc8e2a8697c15331677e6ebf0b",
+            "currentGasLimit": "0x750a163df65e8a",
+            "currentBaseFee": "0x500",
+            "currentNumber": str(block),
+            "currentTimestamp": str(timestamp),
+            "parentTimestamp" : str(parent_timestamp),
+            "parentDifficulty" : hex(parent_diff),
+            "parentUncleHash": parent_uncle_hash
+            }
 
-        data = {}
-        if os.path.exists(output_path):
-            with open(output_path, 'r') as f:
-                data = json.load(f)
+            env_out_path = '/dev/shm/env.json'
+            alloc_out_path = '/dev/shm/alloc.json'
+            txs_out_path = '/dev/shm/txs.json'
+            output_path = '/dev/shm/out.json'
 
-        if not 'currentDifficulty' in data:
-            raise Exception('Difficulty not returned')
+            with open(env_out_path, 'w') as f:
+                json.dump(env, f)
+            with open(alloc_out_path, 'w') as f:
+                json.dump(alloc, f)
+            with open(txs_out_path, 'w') as f:
+                json.dump(txs, f)
 
-        if int(data['currentDifficulty'], 16) == expected_diff:
-            passing_count += 1
-        else:
-            failing_count += 1
-            print('Verification FAIL: Expected {}, Received {}'.format(hex(expected_diff), data['currentDifficulty']))
-        
-        if err:
-            sys.exit()
+            if os.path.exists(output_path):
+                os.remove(output_path)
+
+            proc_output = subprocess.DEVNULL
+            proc_err = subprocess.DEVNULL
+            if err:
+                proc_output = None
+                proc_err = None
+
+            # Run EVM
+            command = ['evm', 't8n', 
+                        '--input.alloc='+alloc_out_path,
+                        '--input.txs='+txs_out_path,
+                        '--input.env='+env_out_path,
+                        '--output.result='+output_path,
+                        '--state.fork='+fork
+                    ]
+            if err:
+                print(command)
+            subprocess.call(command, stdout=proc_output, stderr=proc_err)
+
+            data = {}
+            if os.path.exists(output_path):
+                with open(output_path, 'r') as f:
+                    data = json.load(f)
+
+            if not 'currentDifficulty' in data:
+                raise Exception('Difficulty not returned')
+
+            if int(data['currentDifficulty'], 16) == expected_diff:
+                passing_count += 1
+            else:
+                failing_count += 1
+                print('Verification FAIL: Expected {}, Received {}'.format(hex(expected_diff), data['currentDifficulty']))
+            
+            if err:
+                sys.exit()
         
 if failing_count > 0 or passing_count < test_count:
     print("Verification FAIL: One or more test cases failed, f={} (exp={},other={}), p={}, t={}".format(failing_count, expected_diff_count, failing_count - expected_diff_count, passing_count, test_count))
