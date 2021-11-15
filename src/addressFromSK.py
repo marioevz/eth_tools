ef87ae8d87a9d7bc83d33fd2a49e4e441c277ab6
marioevz <marioevz@gmail.com>
2021-11-15 19:57:49 +0000

    Get address from secret key

---
+++ src/addressFromSK.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+from eth_keys.datatypes import PublicKey
+from eth_keys.backends.native.ecdsa import private_key_to_public_key
+
+def print_usage():
+    print('Usage:\n{} <Secret key>'.format(sys.argv[0]))
+    exit()
+
+if len(sys.argv) != 2:
+    print_usage()
+
+(_, sk) = sys.argv
+
+
+if sk.startswith('0x'):
+    sk = sk[2:]
+
+if (len(sk) % 2) != 0:
+    sk = '0' + sk
+
+sk = bytes.fromhex(sk)
+print('0x' + PublicKey(private_key_to_public_key(sk)).to_canonical_address().hex())
